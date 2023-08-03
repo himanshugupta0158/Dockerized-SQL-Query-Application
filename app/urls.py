9cf2f25dd1d16af951f827cabd4d1420bea9c23f
himanshugupta0158 <93569997+himanshugupta0158@users.noreply.github.com>
2023-08-03 15:26:51 +0530

    updated url

--- app/urls.py
+++ app/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
 from .views import (GetSampleData, LoginView, LogoutView, QueryResult,
-                    SavingData, SignupView, homepage, save_countries_data,
+                    SavingData, SignupView, Homepage, save_countries_data,
                     sendQuery)
 
 urlpatterns = [
     path('save_db_data',SavingData, name="save_db_data"), # Saving data into database model using CSV file from kaggle
     # path('save_countries',save_countries_data, name="save_countries"), # Saving data into database model using CSV file from kaggle
-    path('',homepage.as_view(), name="homepage"),
+    path('',Homepage.as_view(), name="homepage"),
     # path('getdata',getdata.as_view(), name="getdata"),
     path('query',sendQuery.as_view(), name="query"),
     path('query_result',QueryResult.as_view(), name="query_result"),
